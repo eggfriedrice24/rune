daea000bad08f51b02176a762619c14435a689e8
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-05-11 21:00:15 +0100

    remove unused sidebar components

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -4,14 +4,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <TooltipProvider delayDuration={300}>
-        <App />
-      </TooltipProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>,
 );
